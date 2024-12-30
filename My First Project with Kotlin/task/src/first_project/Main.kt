3e2f655cc6e692de157a1d6ed4eb3253638046d2
Adi Trioka <aditrioka@gmail.com>
2024-12-30 08:26:44 +0700

    First commit

---
+++ My First Project with Kotlin/task/src/first_project/Main.kt
@@ -0,0 +1,12 @@
+package first_project
+
+fun main() {
+    // Write your solution here
+    println("Prices")
+    println("Bubblegum: $2")
+    println("Toffee: $0.2")
+    println("Ice cream: $5")
+    println("Milk chocolate: $4")
+    println("Doughnut: $2.5")
+    println("Pancake: $3.2")
+}
